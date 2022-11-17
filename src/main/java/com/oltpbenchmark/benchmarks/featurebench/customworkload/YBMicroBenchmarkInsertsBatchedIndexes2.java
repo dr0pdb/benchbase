2443ee77d0cb97ab612ce61f04c8542bf9a3f54d
RKN Sai Krishna <srangavajjula@yugabyte.com>
2022-11-17 04:34:34 +0000

    Changes to separate setup and the actual tests

---
+++ src/main/java/com/oltpbenchmark/benchmarks/featurebench/customworkload/YBMicroBenchmarkInsertsBatchedIndexes2.java
@@ -0,0 +1,56 @@
+package com.oltpbenchmark.benchmarks.featurebench.customworkload;
+
+import com.oltpbenchmark.benchmarks.featurebench.YBMicroBenchmark;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import org.apache.commons.configuration2.HierarchicalConfiguration;
+import org.apache.commons.configuration2.tree.ImmutableNode;
+import org.apache.log4j.Logger;
+
+public class YBMicroBenchmarkInsertsBatchedIndexes2 extends YBMicroBenchmark {
+  public final static Logger LOG =
+      Logger.getLogger(com.oltpbenchmark.benchmarks.featurebench.customworkload
+                           .YBMicroBenchmarkInsertsBatchedIndexes2.class);
+
+  public YBMicroBenchmarkInsertsBatchedIndexes2(
+      HierarchicalConfiguration<ImmutableNode> config) {
+    super(config);
+    this.loadOnceImplemented = true;
+    this.executeOnceImplemented = true;
+  }
+
+  public void loadOnce(Connection conn) throws SQLException {
+    String insertStmt = "call insert_demo(100);";
+    String DeleteStmt = String.format("delete from demo_indexes_2");
+    PreparedStatement delete_stmt = conn.prepareStatement(DeleteStmt);
+    delete_stmt.execute();
+    delete_stmt.close();
+    PreparedStatement insert_stmt = conn.prepareStatement(insertStmt);
+    insert_stmt.execute();
+    insert_stmt.close();
+  }
+
+  public void executeOnce(Connection conn) throws SQLException {
+    String values = "";
+    for (int i = 101; i <= 1000; i++) {
+      values += "(";
+      for (int col = 1; col <= 11; col++) {
+        values += String.format("%d", i);
+        if (col < 11) {
+          values += ",";
+        }
+      }
+      values += ")";
+      if (i < 1000) {
+        values += ",";
+      }
+    }
+
+    String insertStmt1 = String.format("insert into demo_indexes_2 values %s;", values);
+    Statement stmtObj = conn.createStatement();
+    stmtObj.execute(insertStmt1);
+    stmtObj.close();
+  }
+}
---
+++ src/main/java/com/oltpbenchmark/benchmarks/featurebench/customworkload/YBMicroBenchmarkUpdatesBatchedIndexes5.java
@@ -0,0 +1,53 @@
+package com.oltpbenchmark.benchmarks.featurebench.customworkload;
+
+import com.oltpbenchmark.benchmarks.featurebench.YBMicroBenchmark;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import org.apache.commons.configuration2.HierarchicalConfiguration;
+import org.apache.commons.configuration2.tree.ImmutableNode;
+import org.apache.log4j.Logger;
+
+public class YBMicroBenchmarkUpdatesBatchedIndexes5 extends YBMicroBenchmark {
+  public final static Logger LOG =
+      Logger.getLogger(com.oltpbenchmark.benchmarks.featurebench.customworkload
+                           .YBMicroBenchmarkUpdatesBatchedIndexes5.class);
+  private static final int NUM_ROWS = 1000;
+
+  public YBMicroBenchmarkUpdatesBatchedIndexes5(
+      HierarchicalConfiguration<ImmutableNode> config) {
+    super(config);
+    this.loadOnceImplemented = true;
+  }
+
+  public void loadOnce(Connection conn) throws SQLException {
+    String insertStmt = String.format("call insert_demo(%d);", NUM_ROWS);
+    String DeleteStmt = String.format("delete from demo_indexes_5");
+    PreparedStatement delete_stmt = conn.prepareStatement(DeleteStmt);
+    delete_stmt.execute();
+    delete_stmt.close();
+    PreparedStatement insert_stmt = conn.prepareStatement(insertStmt);
+    insert_stmt.execute();
+    insert_stmt.close();
+  }
+
+  public void executeOnce(Connection conn) throws SQLException {
+    String inClause = "(";
+    for (int i = 101; i <= NUM_ROWS; i++) {
+      inClause += String.format("%d", i);
+      if (i < NUM_ROWS) {
+        inClause += ",";
+      }
+    }
+    inClause += ")";
+
+    // Update last 900 rows.
+    String batchedUpdateStatement = String.format(
+        "update demo_indexes_5 set col1 = col1 + 10000, col2 = col2 + 10000, col3 = col3 + 10000, col4 = col4 + 10000, col5 = col5 + 10000, col6 = col6 + 10000, col7 = col7 + 10000, col8 = col8 + 10000, col9 = col9 + 10000, col10 = col10 + 10000 where id in %s;",
+        inClause);
+    Statement stmtObj = conn.createStatement();
+    stmtObj.execute(batchedUpdateStatement);
+    stmtObj.close();
+  }
+}
